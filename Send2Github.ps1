049646d9d0903c7ec743d041fc20df73c49baa2d
Andrei Ignat <andrei@ignat.se>
2022-02-03 21:01:28 +0100

    Backup

---
+++ Send2Github.ps1
@@ -0,0 +1,12 @@
+﻿# Based on Oneshot Duet Backup and modified by Andrei Ignat
+
+#region user defined
+$workingFolder = ('{0}\DuetBackup' -f [Environment]::GetFolderPath("MyDocuments")) 
+#endregion
+
+#region send to GitHub
+Set-Location -Path $workingFolder
+git add .
+git commit -m "Backup"
+git push -f
+#endregion
